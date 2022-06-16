3279a56f2aeb86d48dd580357bcbe834cb5e63e1
nspbdz <nspbdz@gmail.com>
2022-06-16 18:40:08 +0700

    first

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <hr />
+      <p>AL-Corps - 2022</p>
+      <!-- <p>&copy Copyright Toko Online Powered by Laravel - 2020</p> -->
+    </div>
+  </div>
+</div>
